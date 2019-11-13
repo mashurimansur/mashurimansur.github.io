4bce72886f6d502b85e2d08ac92a281c60493e35
Hury <mashurimansur18@gmail.com>
2019-11-13 17:17:29 +0800

    tes php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>Welcome to my Website</h1>
+    <?php
+        for ($x = 0; $x <= 10; $x++) {
+            echo "The number is: $x <br>";
+        }
+    ?>
 </body>
 </html>
